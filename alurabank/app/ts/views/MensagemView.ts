15cad834409fd2ded216374a27f7793eac598422
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-03-03 15:06:00 -0300

    Typescript project

---
+++ alurabank/app/ts/views/MensagemView.ts
@@ -0,0 +1,8 @@
+import { View } from '../views/index';
+
+
+export class MensagemView extends View<string> {
+    template(model: string): string {
+        return `<p class="alert alert-info">${model}</p>`;
+    }
+}
