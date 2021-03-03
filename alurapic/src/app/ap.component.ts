ee278333d9f98ba0be83b3c03a8eae927581d928
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-03-03 15:26:55 -0300

    angular project

---
+++ alurapic/src/app/ap.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ap-root',
+  templateUrl: './ap.component.html',
+  styleUrls: ['./ap.component.css']
+})
+export class AppComponent { }
---
+++ alurapic/src/app/ap.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './ap.component';
+import { PhotosModule } from './photos/photos.module';
+import { AppRoutingModule } from './ap.routing.module';
+import { ErrorsModule } from './errors/errors.module';
+import { CoreModule } from './core/core.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    PhotosModule,
+    ErrorsModule, 
+    CoreModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
