7a64af1d44cc0378c68c6ee8e86a20a3d57ca07a
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-10-20 08:24:20 -0300

    finished app calculadora

--- formacao-angular/calculadora/src/app/app.component.spec.ts
+++ formacao-angular/calculadora/src/app/app.component.spec.ts
@@ -1,23 +1,24 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { CalculadoraModule } from './calculadora';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  });
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent
+            ],
+            imports: [
+                CalculadoraModule
+            ]
+        }).compileComponents();
+    }));
+  
+    it('should create the app', waitForAsync(() => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
 
 });
