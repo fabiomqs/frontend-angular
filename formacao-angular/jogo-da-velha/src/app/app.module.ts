c40569b18ba2b1892168c90f6a23ede51860987e
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-11-03 06:51:55 -0300

    project Jogo da velha done

--- formacao-angular/jogo-da-velha/src/app/app.module.ts
+++ formacao-angular/jogo-da-velha/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JogoDaVelhaModule } from './jogo-da-velha/jogo-da-velha.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        JogoDaVelhaModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
