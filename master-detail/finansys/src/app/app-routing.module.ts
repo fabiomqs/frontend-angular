e4aa7442c8251994d2c12b1b5da5e92338a4e1ae
Fabio Marques Moreira <fabiomqs@gmail.com>
2021-11-07 06:28:07 -0300

    added categories component

--- master-detail/finansys/src/app/app-routing.module.ts
+++ master-detail/finansys/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    //{ path: 'entries', loadChildren: () => import('./pages/entries/entries.module').then(m => m.EntriesModule) },
+  
+    //{ path: 'reports', loadChildren: () => import('./pages/reports/reports.module').then(m => m.ReportsModule) },
+
+    //{ path: '', redirectTo: '/reports', pathMatch: 'full' }
+    {
+        path: 'categories',
+        loadChildren: () => import('./pages/categories/categories.module')
+                                .then(m => m.CategoriesModule)
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
