18553b0cbd4968e835f52683bdca2e30b4f1fec6
Fabio Marques Moreira <fabiomqs@gmail.com>
2022-03-16 09:15:12 -0300

    add form login

--- formacao-angular-12-com-rest/ponto-inteligente/src/app/autenticacao/login/login.module.ts
+++ formacao-angular-12-com-rest/ponto-inteligente/src/app/autenticacao/login/login.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { LoginComponent } from './components/login/login.component';
 import { LogarComponent } from './components/logar.component';
-import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
     declarations: [
@@ -11,7 +21,16 @@ import { RouterModule } from '@angular/router';
     ],
     imports: [
         CommonModule,
-        RouterModule
+        RouterModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        FlexLayoutModule,
+        MatInputModule,
+        MatButtonModule,
+        MatListModule,
+        MatTooltipModule,
+        MatIconModule,
+        MatSnackBarModule
     ]
 })
 export class LoginModule { }
